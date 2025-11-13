30eb52a4cfdf4ae74f5ca5c6590220f8bec82f84
Farhan Uddin <farhanuddin2006@gmail.com>
2025-11-13 18:39:29 -0500

    added delete and update endpoints, added comments

--- app/app.py
+++ app/app.py
@@ -1,25 +1,74 @@
 from flask import Flask, request, jsonify
-from database_connector import sign_up, get_user
+from database_connector import sign_up, get_user, delete_user, update_username, update_password
 app = Flask(__name__)
 
+'''
+    Endpoint for creating a new user and inserting into the database,
+    returns a message stating sign up success and a user dictionary
+'''
 @app.post('/signup')
 def signup():
     data = request.json
     success = sign_up(data["username"], data["password"], data["email"])
     if not success:
         return jsonify({"Error": "Sign Up Failed, Username or email already exists"}), 400
-    return jsonify({"Message": "Account creation successful!"}), 201
+    return jsonify({"Message": "Account creation successful!",
+                    "User": dict(success)}), 201
 
-
+'''
+    Endpoint for logging into the application, returns a user 
+    dictionary for user logged in as well as a message stating login status
+'''
 @app.post('/login')
 def login():
     data = request.json
     user = get_user(data["username"], data["password"])
     if not user:
         return jsonify({"Error": "Invalid username or password"}), 401
+    
+    #access_token = create_access_token(identity=str(user.user_id))
+
     return jsonify({"Message": "Login Successful!", 
                     "User": dict(user)}), 200
 
+'''
+    Endpoint for deleting a user from the database, returns the messsage 
+    for deletion status
+'''
+@app.post('/delete_user')
+def delete():
+    data = request.json
+    success = delete_user(data["username"], data["password"])
+    if not success:
+        return jsonify({"Error": "Delete Failed, user does not exist"}), 404
+    return jsonify({"Message": "Account delete successful!"}), 200
+
+'''
+    Endpoint for updating username, returns the message for whether the username
+    update was successful or not.
+'''
+@app.post('/update_username')
+def update_user():
+    data = request.json
+    status = update_username(data["old_user"], data["new_user"])
+
+    if status == "user_not_found":
+        return jsonify({"Error": "The user cannot be found"})
+    elif status == "new_username_exists":
+        return jsonify({"Error": "The new username is already taken"})
+    return jsonify({"Message": "Username update successful!"})
+
+'''
+    Endpoint for updating password, returns the message for whether the password
+    update was successful or not.
+'''
+@app.post('/update_password')
+def update_pass():
+    data = request.json
+    success = update_password(data["username"], data["old_password"], data["new_password"])
+    if not success:
+        return jsonify({"Error": "Password update failed"})
+    return jsonify({"Message": "Password update successful!"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-    
--- app/database_connector.py
+++ app/database_connector.py
@@ -1,12 +1,16 @@
-from sqlalchemy import create_engine, MetaData, Table, insert, select
+from sqlalchemy import create_engine, MetaData, Table, insert, select, delete, update
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db_config = {
-    'user': 'uddin',
-    'password': 'BITSMusicAppSecretPassword',
-    'host': 'localhost',
-    'database': 'music_app_db'
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'host': os.getenv("DB_HOST"),
+    'database': os.getenv("DB_DATABASE")
 }
 
 DATABASE_URL = f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
@@ -25,11 +29,10 @@ def sign_up(user, password, email):
         query = insert(users_table).values(username=user, password_hash=password, email=email)
         session.execute(query)
         session.commit()
-        print("Sign Up Successful!")
-        return True
+        return get_user(user, password)
     except IntegrityError:
         session.rollback()
-        return False
+        return None
     finally:
         session.close()
 
@@ -39,5 +42,44 @@ def get_user(user, password):
         query = select(users_table).where(users_table.c.username == user, users_table.c.password_hash == password)
         result = session.execute(query).mappings().fetchone()
         return result if result else None
+    finally:
+        session.close()
+
+def delete_user(user, password):
+    session = get_session()
+    try:
+        query = delete(users_table).where(users_table.c.username == user, users_table.c.password_hash == password)
+        result = session.execute(query)
+        session.commit()
+        return result.rowcount > 0
+    finally:
+        session.close()
+
+def update_username(old_user, new_user):
+    session = get_session()
+    try:
+        query = update(users_table).where(users_table.c.username == old_user).values(username=new_user)
+        result = session.execute(query)
+        if result.rowcount == 0:
+            session.rollback()
+            return "user_not_found"
+        session.commit()
+        return "success"
+    except IntegrityError:
+        session.rollback()
+        return "new_username_exists"
+    finally:
+        session.close()
+
+def update_password(username, old_password, new_password):
+    session = get_session()
+    try:
+        query = update(users_table).where(users_table.c.username == username, users_table.c.password_hash == old_password).values(password_hash=new_password)
+        result = session.execute(query)
+        session.commit()
+        return result.rowcount > 0
+    except Exception:
+        session.rollback()
+        return False
     finally:
         session.close()
